089b3c035d9d782306afafb15f2db16724e62ce5
bzp2010 <bzp2010@apache.org>
2023-01-29 15:22:45 +0800

    chore: change V alias

--- lib/resty/ldap/filter.lua
+++ lib/resty/ldap/filter.lua
@@ -26,8 +26,6 @@ local function pack(...)
 end
 
 -- Utility
-local _= V
-
 local function maybe(pattern)
     if type(pattern) == 'string' then pattern = V(pattern) end
     return pattern ^ -1
@@ -61,39 +59,39 @@ local rawValue = (P'_' + R('az', 'AZ')) * (P'_' + R '09' + R('az', 'AZ')) ^ 0
 
 -- Grammar
 local filter = P{
-    _'FILTER' / function(f)
+    V'FILTER' / function(f)
         return f
     end,
-    FILTER = _'FILL' * P'(' * _'OP' * P')' * _'FILL' / function(f)
+    FILTER = V'FILL' * P'(' * V'OP' * P')' * V'FILL' / function(f)
         return f
     end,
 
-    OP= (_'OP_AND' + _'OP_OR' + _'OP_NOT' + _'ITEM'),
-    OP_AND = (P'&' * _'FILL' * list('FILTER', 1) * _'FILL') / function(...)
+    OP= (V'OP_AND' + V'OP_OR' + V'OP_NOT' + V'ITEM'),
+    OP_AND = (P'&' * V'FILL' * list('FILTER', 1) * V'FILL') / function(...)
         return cOPBody(_M.OP_TYPE_AND, ...)
     end,
-    OP_OR = (P'|' * _'FILL' * list('FILTER', 1) * _'FILL') / function(...)
+    OP_OR = (P'|' * V'FILL' * list('FILTER', 1) * V'FILL') / function(...)
         return cOPBody(_M.OP_TYPE_OR, ...)
     end,
     OP_NOT =
-        (P'!' * _'FILL' * _'FILTER' * _'FILL') / function(...)
+        (P'!' * V'FILL' * V'FILTER' * V'FILL') / function(...)
             return cOPBody(_M.OP_TYPE_NOT, ...)
         end,
-    ITEM = _'ITEM_SUBSTRING' +  _'ITEM_SIMPLE' + _'ITEM_PRESENT',
-    ITEM_SUBSTRING = (_'ATTRIBUTE_DESCRIPTION' * _'FILTER_TYPE_EQUAL' * _'ATTRIBUTE_VALUE_SUBSTRING') / function(...)
+    ITEM = V'ITEM_SUBSTRING' +  V'ITEM_SIMPLE' + V'ITEM_PRESENT',
+    ITEM_SUBSTRING = (V'ATTRIBUTE_DESCRIPTION' * V'FILTER_TYPE_EQUAL' * V'ATTRIBUTE_VALUE_SUBSTRING') / function(...)
         return cItemBody(_M.ITEM_TYPE_SUBSTRING, ...)
     end,
-    ITEM_SIMPLE = _'ATTRIBUTE_DESCRIPTION' * _'FILTER_TYPE' * _'ATTRIBUTE_VALUE' / function(...)
+    ITEM_SIMPLE = V'ATTRIBUTE_DESCRIPTION' * V'FILTER_TYPE' * V'ATTRIBUTE_VALUE' / function(...)
         return cItemBody(_M.ITEM_TYPE_SIMPLE, ...)
     end,
-    ITEM_PRESENT = _'ATTRIBUTE_DESCRIPTION' * P'=*' / function(value)
+    ITEM_PRESENT = V'ATTRIBUTE_DESCRIPTION' * P'=*' / function(value)
         return cItemBody(
             _M.ITEM_TYPE_PRESENT, value,
             { filter_type = _M.FILTER_TYPE_EQUAL },
             { attribute_value = "*" }
         )
     end,
-    FILTER_TYPE = _'FILTER_TYPE_EQUAL' + _'FILTER_TYPE_APPROX' + _'FILTER_TYPE_GREATER' + _'FILTER_TYPE_LESS',
+    FILTER_TYPE = V'FILTER_TYPE_EQUAL' + V'FILTER_TYPE_APPROX' + V'FILTER_TYPE_GREATER' + V'FILTER_TYPE_LESS',
     FILTER_TYPE_EQUAL = P'=' / function()
         return { filter_type = _M.FILTER_TYPE_EQUAL }
     end,
@@ -114,9 +112,9 @@ local filter = P{
         return { attribute_value = value }
     end,
     ATTRIBUTE_VALUE_SUBSTRING =
-        ((_'ATTRIBUTE_VALUE' * _'WILDCARD') +
-        (_'WILDCARD' * _'ATTRIBUTE_VALUE' * _'WILDCARD') +
-        (_'WILDCARD' * _'ATTRIBUTE_VALUE')) / function(...)
+        ((V'ATTRIBUTE_VALUE' * V'WILDCARD') +
+        (V'WILDCARD' * V'ATTRIBUTE_VALUE' * V'WILDCARD') +
+        (V'WILDCARD' * V'ATTRIBUTE_VALUE')) / function(...)
             local s = {}
             for _, value in ipairs({...}) do
                 table_insert(s, type(value) == "table" and value.attribute_value or value)
@@ -125,12 +123,12 @@ local filter = P{
         end,
 
     WILDCARD = P'*' / '*',
-    FILL = list(_'SPACE' + _'TAB' + _'SEP', 0) / function() end,
-    SPACE = P(string_char(0x20)),
-    TAB = P(string_char(0x09)),
-    SEP= (_'CR' * _'LF') + _'CR' + _'LF',
-    CR = P'\r',
-    LF = P'\n',
+    FILL = list(V'SPACE' + V'TAB' + V'SEP', 0) / function() end,
+    SPACE = P(string_char(0x20)),         -- ASCII control charactor - Space
+    TAB = P(string_char(0x09)),           -- ASCII control charactor - TAB
+    SEP= (V'CR' * V'LF') + V'CR' + V'LF', -- ASCII control charactor - CR/LF/CRLF
+    CR = P'\r',                           -- ASCII control charactor - CR
+    LF = P'\n',                           -- ASCII control charactor - LF
 }
 
 
